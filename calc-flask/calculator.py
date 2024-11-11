78c30d1bca26ce7026deef6a9899815032df8c88
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-11 15:15:40 +0100

    added power, modulo, sqrt to calculator

--- calc-flask/calculator.py
+++ calc-flask/calculator.py
@@ -24,3 +24,28 @@ class Calculator:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         self.result = num1 / num2
         return self.result
+    
+    
+    def power(self, num1: float, num2: float)-> float:
+        self.result = num1 ** num2
+        return self.result
+    
+
+    def modulo(self, num1: float, num2: float)-> float:
+        self.result = num1 % num2
+        return self.result
+    
+    
+    def sqrt(self, num1: float)-> float:
+        """Calculate the square root of a non-negative number.
+        Parameters:
+            num1 (float): A non-negative number to calculate the square root of.
+        Returns:
+            float: The square root of num1.
+        Raises:
+            ValueError: If num1 is negative.
+        """
+        if num1 < 0:
+            raise ValueError("Cannot calculate the square root of a negative number.")
+        self.result = sqrt(num1)
+        return self.result
